2ca32818c09130f9371e5c97e07c6c1290dfc3db
Tanisha Rajgor <tanisha.rajgor@gmail.com>
2023-08-21 19:51:09 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,32 @@
+import {
+  canvas,
+  backgroundImage,
+  drawBird,
+  drawPipe,
+  update,
+  gameLoop,
+  resetGame,
+  bird,
+  pipes,
+  score,
+  gameOver,
+} from './script.js';
+
+backgroundImage.onload = () => {
+  gameLoop();
+};
+
+playAgainButton.addEventListener("click", () => {
+  resetGame();
+  gameLoop();
+});
+
+document.addEventListener("keydown", (event) => {
+  if (event.keyCode === 32) {
+    if (!gameOver) {
+      bird.velocityY = bird.jumpPower;
+    } else {
+      startGame();
+    }
+  }
+});
